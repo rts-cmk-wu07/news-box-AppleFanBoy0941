7ed5539070b7fc6e183d9863ea22fa3d688c5cbb
Sebastian Aage Nielsen <u33674@rts-365.dk>
2022-06-09 11:10:20 +0200

    variables setup

---
+++ src/variables/variables.js
@@ -0,0 +1,36 @@
+export const variables = {
+	light: {
+		primary_1: '#87BCBF',
+		primary_2: '#D97D54',
+		primary_3: '#324755',
+		primary_4: '#D95454',
+		secondary_1: '#F0F3F4',
+		secondary_2: '#C8D1D3',
+		secondary_3: '#B9B0A2',
+		text_1: '#1B1C20',
+		text_2: '#6E8CA0',
+		text_3: '#FFFFFF',
+
+		br_l: '2rem',
+		br_s: '.5rem',
+
+		subtle_shadow: '0 0.5rem 1.5rem',
+	},
+	dark: {
+		primary_1: '#87BCBF',
+		primary_2: '#D97D54',
+		primary_3: '#324755',
+		primary_4: '#D95454',
+		secondary_1: '#F0F3F4',
+		secondary_2: '#C8D1D3',
+		secondary_3: '#B9B0A2',
+		text_1: '#1B1C20',
+		text_2: '#6E8CA0',
+		text_3: '#FFFFFF',
+
+		br_l: '2rem',
+		br_s: '.5rem',
+
+		subtle_shadow: '0 0.5rem 1.5rem',
+	},
+};
