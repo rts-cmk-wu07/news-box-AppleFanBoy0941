6c9bebd78960bd15de7b4f472807b936a6ea67f3
Sebastian Aage Nielsen <u33674@rts-365.dk>
2022-06-09 11:21:39 +0200

    React router dom setup

--- src/index.js
+++ src/index.js
@@ -2,10 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Archive from './pages/Archive';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<App />}>
+					<Route path="/home" element={<Home />} />
+					<Route path="/archive" element={<Archive />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+		<App />
+	</React.StrictMode>
 );
---
+++ src/pages/Archive.js
@@ -0,0 +1,5 @@
+const Archive = () => {
+	return <main></main>;
+};
+
+export default Archive;
