bbe91d3c506efa8089e24ea6ceef05c86cd1b2f3
Sebastian Aage Nielsen <u33674@rts-365.dk>
2022-06-10 09:04:45 +0200

    added fetch hook

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react';
+
+const useFetch = url => {
+	const [data, setData] = useState(null);
+	const [isPending, setIsPending] = useState(true);
+	const [error, setError] = useState(null);
+
+	useEffect(() => {
+		setTimeout(() => {
+			fetch(url)
+				.then(res => {
+					if (!res.ok) {
+						throw Error('Could not fetch the data');
+					}
+
+					return res.json();
+				})
+				.then(data => {
+					setIsPending(false);
+					setData(data);
+					setError(null);
+				})
+				.catch(err => {
+					setError(err.message);
+					setIsPending(false);
+				});
+		}, 1000);
+	}, [url]);
+
+	return { data, isPending, error };
+};
+
+export default useFetch;
